848dd8143ba144486c6db84c5a62028fd6285121
zhangjiahao <zhangjiahao@bhxz.net>
2023-07-27 10:29:54 +0800

    ring_queue: 实现未定义的成员函数

--- RingQueue/src/ring_queue.cpp
+++ RingQueue/src/ring_queue.cpp
@@ -1,12 +1,14 @@
 #include "ring_queue.h"
 
+bool RingQueue::isEmpty()
+{
+    return m_headIndex == m_tailIndex;
+}
+
 bool RingQueue::isFull()
 {
-    if((m_tailIndex + 1) % m_maxCapacity == m_headIndex) {
-        return true;
-    }
-
-    return false;
+    // 队列为满时，读索引和写索引之间空一个位置
+    return (m_tailIndex + 1) % m_maxCapacity == m_headIndex;
 }
 
 void RingQueue::enQueue(int value)
@@ -14,8 +16,11 @@ void RingQueue::enQueue(int value)
     if(isFull()) {
         m_headIndex = (m_headIndex + 1) % m_maxCapacity;
     }
+
     m_dataBuf[m_tailIndex] = value;
+
     std::cout << "Push to queue : " << m_dataBuf[m_tailIndex] << std::endl;
+
     m_tailIndex = (m_tailIndex + 1) % m_maxCapacity;
 }
 
@@ -35,9 +40,21 @@ void RingQueue::showQueue()
 
 void RingQueue::deQueue()
 {
+    if(isEmpty()) {
+        std::cout << "Queue is empty !" << std::endl;
+        return;
+    }
+
     std::cout << "Pop from queue : ";
+
     std::cout << m_dataBuf[m_headIndex] << std::endl;
-    m_headIndex = (m_headIndex + 1) % m_maxCapacity; 
+
+    m_headIndex = (m_headIndex + 1) % m_maxCapacity;
+}
+
+void RingQueue::showQueueHead()
+{
+    std::cout << "Queue head is : " << m_dataBuf[m_headIndex] << std::endl;
 }
 
 int main()
